07a5e7210c0149bec17512bd2e5248eca91b56d7
OsvaldoReTo <resendiz.tolentino.osvaldo@gmail.com>
2022-09-28 15:47:49 -0500

    chore | Se agregaron elementos a gitignore

--- src/testFiguras.java
+++ src/testFiguras.java
@@ -18,10 +18,10 @@ public class testFiguras {
 	
 	public static void imprimirCalculo(figuraGeometrica t) {
 		System.out.println(t);
-		System.out.println("+=========================================================");
+		System.out.println("+==============================================================================================");
 		System.out.println("|El área de ["+t.getName()+"] es: "+ t.calcularArea() +
 				"\n" + "|El perímetro de ["+t.getName()+"] es: " + t.calcularPerimetro());
-		System.out.println("+=========================================================");
+		System.out.println("+==============================================================================================");
 	}
 
 }// class testFiguras
